c23659fcbebf4f6397371a27241ec7ac769295f0
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 19:56:47 +1030

    add bstats metrics

--- src/main/java/me/shanodekono/nostrip/NoStrip.java
+++ src/main/java/me/shanodekono/nostrip/NoStrip.java
@@ -1,11 +1,15 @@
 package me.shanodekono.nostrip;
 
 import me.shanodekono.nostrip.listeners.PlayerInteractListener;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoStrip extends JavaPlugin {
     @Override
     public void onEnable() {
+        //noinspection unused
+        Metrics metrics = new Metrics(this);
+
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
     }
 }
