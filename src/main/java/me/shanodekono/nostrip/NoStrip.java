92f8d629009087795123235840e2fb04a3ba0d39
Haileykins <hailey.loralyn@gmail.com>
2018-10-20 00:37:31 -0700

    Update, Clean Up, And Polish NoStrip Class

--- src/main/java/me/shanodekono/nostrip/NoStrip.java
+++ src/main/java/me/shanodekono/nostrip/NoStrip.java
@@ -2,27 +2,24 @@ package me.shanodekono.nostrip;
 
 import me.shanodekono.nostrip.commands.NoStripCommand;
 import me.shanodekono.nostrip.listeners.PlayerInteractListener;
+import me.shanodekono.nostrip.utils.ConfigUtils;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-public class NoStrip extends JavaPlugin implements Listener {
-
-private static List<UUID> enabledPlayers = new ArrayList<>();
+public class NoStrip extends JavaPlugin {
 
     @Override
     public void onEnable() {
         //noinspection unused
         Metrics metrics = new Metrics(this);
 
-        getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
-        this.getCommand("nostrip").setExecutor(new NoStripCommand());
+        ConfigUtils cfgUtils = new ConfigUtils(this);
+        NoStripCommand nsCommand = new NoStripCommand(cfgUtils);
+
+        cfgUtils.loadConfig();
+
+        getServer().getPluginManager().registerEvents(new PlayerInteractListener(nsCommand), this);
+        getCommand("nostrip").setExecutor(new NoStripCommand(cfgUtils));
     }
-    public static List<UUID> getEnabledPlayers() {
-        return enabledPlayers;
-}
+
 }
