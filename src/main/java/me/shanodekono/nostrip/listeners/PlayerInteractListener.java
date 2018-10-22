3c7cba47be1e19b49339c82d3f48bf94def596a8
Haileykins <hailey.loralyn@gmail.com>
2018-10-22 12:30:04 -0700

    Rename axes to axePermissions, create list axes to verify item in hand to prevent NPE from erroring out in the console

--- src/main/java/me/shanodekono/nostrip/listeners/PlayerInteractListener.java
+++ src/main/java/me/shanodekono/nostrip/listeners/PlayerInteractListener.java
@@ -15,14 +15,16 @@ import java.util.Map;
 public class PlayerInteractListener implements Listener {
 
     private List<Material> logs;
-    private Map<Material, String> axes;
+    private List<Material> axes;
+    private Map<Material, String> axePermissions;
 
     private ConfigUtils cfgUtils;
 
     public PlayerInteractListener(ConfigUtils configUtils) {
 
         logs = new ArrayList<>();
-        axes = new HashMap<>();
+        axes = new ArrayList<>();
+        axePermissions = new HashMap<>();
 
         logs.add(Material.ACACIA_LOG);
         logs.add(Material.BIRCH_LOG);
@@ -31,11 +33,17 @@ public class PlayerInteractListener implements Listener {
         logs.add(Material.SPRUCE_LOG);
         logs.add(Material.OAK_LOG);
 
-        axes.put(Material.WOODEN_AXE, "nostrip.axe.wood");
-        axes.put(Material.STONE_AXE, "nosttrip.axe.stone");
-        axes.put(Material.IRON_AXE, "nostrip.axe.iron");
-        axes.put(Material.GOLDEN_AXE, "nostrip.axe.gold");
-        axes.put(Material.DIAMOND_AXE, "nostrip.axe.diamond");
+        axes.add(Material.WOODEN_AXE);
+        axes.add(Material.STONE_AXE);
+        axes.add(Material.IRON_AXE);
+        axes.add(Material.GOLDEN_AXE);
+        axes.add(Material.DIAMOND_AXE);
+
+        axePermissions.put(Material.WOODEN_AXE, "nostrip.axe.wood");
+        axePermissions.put(Material.STONE_AXE, "nosttrip.axe.stone");
+        axePermissions.put(Material.IRON_AXE, "nostrip.axe.iron");
+        axePermissions.put(Material.GOLDEN_AXE, "nostrip.axe.gold");
+        axePermissions.put(Material.DIAMOND_AXE, "nostrip.axe.diamond");
 
         cfgUtils = configUtils;
     }
@@ -54,6 +62,10 @@ public class PlayerInteractListener implements Listener {
             return;
         }
 
+        if (!axes.contains(event.getPlayer().getInventory().getItemInMainHand().getType())) {
+            return;
+        }
+
         /*
         NOTE: Toggle Contains The Players Who Want To Prevent Stripping, So We Want To Check If They Are Not In Toggle
         Because That Means They WANT To Strip Logs. If It's Determined That They Want To Strip Logs We Verify They Have
@@ -62,7 +74,7 @@ public class PlayerInteractListener implements Listener {
         if (!cfgUtils.toggle.contains(event.getPlayer().getUniqueId())) {
 
             // If They Have An Individual Axe Permission For The Axe They Are Using, Let Them Strip The Log
-            if (event.getPlayer().hasPermission(axes.get(event.getItem().getType()))) {
+            if (event.getPlayer().hasPermission(axePermissions.get(event.getItem().getType()))) {
                 return;
             }
 
